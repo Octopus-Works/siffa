01dabfb9eb661dbbc5b6bc590f587d4c9ca7f236
amjadalfawal <aamghed@gmail.com>
2019-04-22 12:50:17 +0300

    Update about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('incs.head')
+
+<style>
+
+@media (max-width: 740px)
+{
+	html, body, header, .view {
+	height: unset !important;
+}
+
+}
+
+</style>
+</head>
+
+
+<body class="creative-lp">
+
+
+    <!--Main Navigation-->
+    <header>
+        @include('incs.nav')
+        <div class="view jarallax" style="height: 100vh;">
+            <img class="jarallax-img" src="https://mdbootstrap.com/img/Photos/Others/img%20%2844%29.jpg" alt="">
+            <div class="mask rgba-blue-slight">
+                <div class="container flex-center text-center">
+                    <div class="row mt-5">
+                        <div class="col-md-12 wow fadeIn mb-3">
+                            <h1 class="display-3 mb-2 wow fadeInDown" data-wow-delay="0.3s">About us</h1>
+                            <h5 class="text-uppercase mb-3 mt-1 font-weight-bold wow fadeIn" data-wow-delay="0.4s">Siffa</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--Main Navigation-->
+
+
+
+<main>
+
+	<div class="container">
+
+		<!-- Section: About 1-->
+		<section id="about" class="section about mt-5 mb-5 py-3 wow fadeIn" data-wow-delay="0.2s">
+				<h5 class="black-text description mb-5 wow fadeIn" data-wow-delay="0.4s">
+						Founded in 2019 by  Tournoud, siffa is an
+						exclusive line of transpotion created to embellish and enhance the look of any
+						cars.
+					</h5>
+			<!-- Grid row -->
+			<div class="row pt-2 mt-5">
+
+				<!-- Grid column -->
+				<div class="col-lg-5 col-md-12 mb-5 wow fadeIn" data-wow-delay="0.4s">
+					<!-- Image -->
+					<img src="https://mdbootstrap.com/img/Photos/Others/images/72.jpg"
+						 class="img-fluid z-depth-1 rounded" alt="My photo">
+
+					<p align="center" class="grey-text my-3">
+						<SPAN style="color :#4a4a4a !important ;"><strong></strong></SPAN>
+					<p align="justify" class="grey-text my-3">
+						<br>We say…
+						<br>Yes to spreading your wings, and soaring.
+						<br>Yes to taking your time, and transforming into a butterfly.
+						<br>Yes to being fierce, and to pursuing what sets your soul on fire.
+						<br>Yes to embracing your uniqueness, and to being different.
+						<br>Yes to accepting change, and staying faithful to yourself.
+						<br>Yes to remaining a flawless yet imperfect version of your own true self.
+						<br>Yes to being you, the best you.
+					</p>
+					</p>
+				</div>
+				<!-- Grid column -->
+
+				<!-- Grid column -->
+				<div class="col-lg-6 ml-auto col-md-12 wow fadeIn" data-wow-delay="0.4s">
+
+					<!-- Secion heading -->
+					<h3 class="mb-2 dark-grey-text title font-weight-light wow fadeIn" data-wow-delay="0.2s">
+						<strong>siffa is a project by women, for women.</strong>
+					</h3>
+
+					<!-- Description -->
+					<p align="justify" class="grey-text"><strong>siffa </strong>specializes in handmade leather
+						transpotion with innovative designs, and new-age techniques used to customize women’s
+						carss.The distinctive transpotion can easily be attached to any type of bag, to personalize
+						the piece according to one’s personality and mood of the moment.</p>
+
+					<p align="justify" class="grey-text"><strong>siffa </strong>celebrates individuality - and
+						through its state-of-the-art products, aspires to magnify beauty, embrace uniqueness, and to
+						take pride in individual personas. In a world of modernization and mass-production, fast-tracked
+						by the digital era, it’s easy for one to lose their true sense of self. The unspoken pressures
+						of keeping up appearances, has raised an unfair set of rules, resulting in the loss of
+						individuals’ uniqueness, and the priceless value of the latter.</p>
+
+					<p align="justify" class="grey-text"><strong>siffa </strong>believes in in the power of
+						personality, and aims to make beautiful creations, to appreciate the individuality and
+						uniqueness of all women. With our line of transpotion, we fight against social pressures, we
+						challenge women to be different, to be unique, to simply be them-selves.</p>
+
+
+				</div>
+				<!-- Grid column -->
+
+			</div>
+			<!-- Grid row -->
+
+		</section>
+		<!-- Section: About 1-->
+
+		<hr>
+
+		<!-- Section: About 2-->
+		<section id="about" class="section about mt-5 mb-5 pb-3 wow fadeIn" data-wow-delay="0.2s">
+
+            <div class="row pt-4 mt-5">
+
+                <!-- Grid column -->
+                <div class="col-lg-6 col-md-12 wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.4s;">
+
+                    <!-- Secion heading -->
+                    <h3 class="mb-5 dark-grey-text title font-weight-bold wow fadeIn" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
+                        <strong>Our mission</strong>
+                    </h3>
+
+                    <!-- Description -->
+                    <p align="justify" class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo animi soluta ratione quisquam, dicta
+                        ab cupiditate iure eaque? Repellendus voluptatum, magni impedit delectus, beatae maxime temporibus
+                        maiores quibusdam.</p>
+                    <p align="justify" class="grey-text">Rem magnam ad perferendis iusto sint tempora ea voluptatibus iure, animi excepturi modi aut possimus
+                        in hic molestias repellendus illo ullam odit quia velit. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit.</p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-lg-6 col-md-12 mb-3 wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.4s;">
+
+                    <div class="text-left">
+                        <h6 class="feature-title title font-weight-bold dark-grey-text  spacing mt-4 mx-4">
+                            <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                            <strong>Lorem ipsum dolor sit amet</strong>
+                        </h6>
+                        <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred.
+                        </p>
+                    </div>
+
+                    <div class="text-left">
+                        <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                            <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                            <strong>Lorem ipsum dolor sit amet</strong>
+                        </h6>
+                        <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                        </p>
+                    </div>
+
+                    <div class="text-left">
+                        <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                            <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                            <strong>Lorem ipsum dolor sit amet</strong>
+                        </h6>
+                        <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                        </p>
+                    </div>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+
+		</section>
+		<!-- Section: About 2-->
+        <hr>
+			<!-- Section: About 2-->
+            <section id="about" class="section about mt-5 mb-5 pb-3 wow fadeIn" data-wow-delay="0.2s">
+
+                <div class="row pt-4 mt-5">
+    
+
+                    <!-- Grid column -->
+    
+                    <!-- Grid column -->
+                    <div class="col-lg-6 col-md-12 mb-3 wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.4s;">
+    
+                        <div class="text-left">
+                            <h6 class="feature-title title font-weight-bold dark-grey-text  spacing mt-4 mx-4">
+                                <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                <strong>Lorem ipsum dolor sit amet</strong>
+                            </h6>
+                            <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred.
+                            </p>
+                        </div>
+    
+                        <div class="text-left">
+                            <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                                <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                <strong>Lorem ipsum dolor sit amet</strong>
+                            </h6>
+                            <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                            </p>
+                        </div>
+    
+                        <div class="text-left">
+                            <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                                <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                <strong>Lorem ipsum dolor sit amet</strong>
+                            </h6>
+                            <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                            </p>
+                        </div>
+    
+                    </div>
+                    <!-- Grid column -->
+                        <!-- Grid column -->
+                        <div class="col-lg-6 col-md-12 wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.4s;">
+    
+                            <!-- Secion heading -->
+                            <h3 class="mb-5 dark-grey-text title font-weight-bold wow fadeIn" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
+                                <strong>Our Vision</strong>
+                            </h3>
+        
+                            <!-- Description -->
+                            <p align="justify" class="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo animi soluta ratione quisquam, dicta
+                                ab cupiditate iure eaque? Repellendus voluptatum, magni impedit delectus, beatae maxime temporibus
+                                maiores quibusdam.</p>
+                            <p align="justify" class="grey-text">Rem magnam ad perferendis iusto sint tempora ea voluptatibus iure, animi excepturi modi aut possimus
+                                in hic molestias repellendus illo ullam odit quia velit. Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit.</p>
+        
+                        </div>
+                </div>
+        <hr>
+            </section>
+            <!-- Section: About 2-->
+
+		<h2 class="h1-responsive font-weight-light my-5 text-center ">
+            President of the Association Letter
+				<span class="grey-text"><br><small><small>SIFFA</small></small></span>
+			</h2>
+			<hr style="width:50%">
+		<!-- Section: Testimonials v.2 -->
+		{{-- <section class="text-center my-5 "> --}}
+				<section id="about" class="section py-5">
+
+						<!-- First row -->
+						<div class="row mt-5 wow fadeIn" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
+		
+							<!-- Place for photo -->
+							<div class="col-lg-4 mb-5 flex-center">
+								<div class="view z-depth-1">
+									<img src="https://mdbootstrap.com/img/Photos/Others/model3.jpg" class="img-fluid z-depth-1" alt="">
+								</div>
+							</div>
+		
+							<!-- Text content -->
+							<div class="col-lg-7 ml-3">
+								<h3 class="font-weight-light"> President of the Association </h3>
+								<h6 style="color: #af8b2e !important;" class="font-weight-light my-3">letter</h6>
+		
+														<hr>
+								<p align="justify" class="mt-4"><span>Angelique Tournoud, is quintessentially French in all her ways, with an inherited elegance, and an eye for detail. Inspired by her graceful mother, who introduced her to beautiful fabrics and the art of traditional savoir-faire, Angelique developed an appreciation for beautiful things from a young age.
+		
+										An idealist and a perfectionist, yet always creative, she was completely captivated by Zina’s idea to amplify & personalize women’s carss. Following lengthy research, countless illustrations, design trials, and endless hours of deliberation over the selection of symbols - dream turned into reality, with the completion of a collection real-size bag transpotion.
+										Through their concept and creations, Zina & Angelique highlight the importance of individualism, symbolized in high quality transpotion, to personalize a woman’s cars.
+								</span></p>
+		
+							</div>
+		
+						</div>
+						<!-- /.First row -->
+		
+
+						<hr>
+						<!-- First row -->
+		
+                    </section>
+
+                    
+                    <section id="about1" class="section about mb-5 py-3 wow fadeIn" data-wow-delay="0.2s">
+                            <h3 class="mb-5 dark-grey-text title font-weight-bold wow fadeIn text-center" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
+                                    <strong>Trading Standards</strong>
+                                </h3>
+                                <hr class="w-50">
+                        <div class="row pt-4 mt-1">
+         
+                
+            
+                            <!-- Grid column -->
+                            <div class="col-lg-12 col-md-12 mb-3 wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.4s;">
+            
+                                <div class="text-left">
+                                    <h6 class="feature-title title font-weight-bold dark-grey-text  spacing mt-4 mx-4">
+                                        <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                        <strong>Lorem ipsum dolor sit amet</strong>
+                                    </h6>
+                                    <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred.
+                                    </p>
+                                </div>
+            
+                                <div class="text-left">
+                                    <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                                        <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                        <strong>Lorem ipsum dolor sit amet</strong>
+                                    </h6>
+                                    <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                                    </p>
+                                </div>
+            
+                                <div class="text-left">
+                                    <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                                        <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                        <strong>Lorem ipsum dolor sit amet</strong>
+                                    </h6>
+                                    <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                    </p>
+                                </div>
+            
+                            </div>
+                            <!-- Grid column -->
+            
+                        </div>
+            
+                    </section>
+
+                    <hr >
+                    <section id="about2" class="section about mb-5 py-3 wow fadeIn" data-wow-delay="0.2s">
+                            <h3 class="mb-5 dark-grey-text title font-weight-bold wow fadeIn text-center" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
+                                    <strong>Laws</strong>
+                                </h3>
+                                <hr class="w-50">
+                        <div class="row pt-4 mt-1">
+         
+                
+            
+                            <!-- Grid column -->
+                            <div class="col-lg-12 col-md-12 mb-3 wow fadeIn" data-wow-delay="0.4s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.4s;">
+            
+                                <div class="text-left">
+                                    <h6 class="feature-title title font-weight-bold dark-grey-text  spacing mt-4 mx-4">
+                                        <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                        <strong>Lorem ipsum dolor sit amet</strong>
+                                    </h6>
+                                    <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred.
+                                    </p>
+                                </div>
+            
+                                <div class="text-left">
+                                    <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                                        <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                        <strong>Lorem ipsum dolor sit amet</strong>
+                                    </h6>
+                                    <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                                    </p>
+                                </div>
+            
+                                <div class="text-left">
+                                    <h6 class="feature-title title font-weight-bold dark-grey-text spacing mt-4 mx-4">
+                                        <i class="fas fa-square orange-text mr-2" aria-hidden="true"></i>
+                                        <strong>Lorem ipsum dolor sit amet</strong>
+                                    </h6>
+                                    <p class="grey-text font-small mx-4">Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                    </p>
+                                </div>
+            
+                            </div>
+                            <!-- Grid column -->
+            
+                        </div>
+            
+                    </section>
+
+                    <hr class="mt-5 mb-5" >
+
+	</div>
+
+
+
+		<!--Second container-->
+
+</main>
+
+
+<div class="container">
+
+</div>
+
+</div>
+
+
+
+
+
+@include('incs.footer')
+
+</body>
+
+</html>
+
