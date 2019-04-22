ff88a3e49df680432669b663c8e5553992f2d8ee
YazanKhatib <yazankhatib97@gmail.com>
2019-04-22 09:38:12 +0300

    Creating about view & route.

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('pages/about');
+});
+
 Route::get('/reports', function () {
     return view('pages/reports&publications');
 });
