414d57d4f7ec3c5f5c0e1d277edd7fbc47adee45
dana98k <40704503+dana98k@users.noreply.github.com>
2019-07-10 11:13:27 +0300

    Update verificationemail.blade.php

--- resources/views/vendor/notifications/verificationemail.blade.php
+++ resources/views/vendor/notifications/verificationemail.blade.php
@@ -1,23 +1,15 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+
 </head>
 
-<body class="text-center">
-    <div class="card m-auto" style="width:50%; border:1px solid blue">
-        <div class="row">
-            <div class="col-12">
-                <img style="
-                height: auto !important;
-                width: 150px !important;" class="mt-4" src="{{url('/img/Logos/logo_xs_1.png')}}" />
-            </div>
-        </div>
-        <h3 class="mt-3 mb-3"> Your login credentials are: </h3>
-        <div class="mb-3">
-            <p class="dark-grey-text">Username: {{$username }} </p>
-            <p class="dark-grey-text">Password: {{$password }}</p>
-        </div>
-    </div>
+<body>
+    <img style="height: auto; width: 150px" src="{{url('img/Logos/logo_xs_1.png')}}" />
+    <h3> Your login credentials are: </h3>
+    <p>Username: {{$username }} </p>
+    <p>Password: {{$password }}</p>
 </body>
 
 </html>
